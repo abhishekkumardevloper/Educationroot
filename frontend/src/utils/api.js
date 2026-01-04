9b875ad2d45123f7d2e94a99e5607bd9f05bb08d
abhishekkumardevloper <abhishekkumar.devloper@gmail.com>
2026-01-04 22:12:33 +0530

    Update API configuration and headers
    Refactor API configuration to use VITE_API_URL and update headers.

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,18 +1,11 @@
-import axios from 'axios';
-
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = `${BACKEND_URL}/api`;
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: API,
-});
-
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: false,
 });
 
 export default api;
